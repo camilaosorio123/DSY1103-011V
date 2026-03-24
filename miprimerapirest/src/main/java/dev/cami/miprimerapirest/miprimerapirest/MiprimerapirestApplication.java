e3ad3eb9c85b12d35723043dc01787bc3c671212
Camila <camil.osorior@duocuc.cl>
2026-03-24 12:05:01 -0300

    GREETINGS FORMAL

---
+++ miprimerapirest/src/main/java/dev/cami/miprimerapirest/miprimerapirest/MiprimerapirestApplication.java
@@ -0,0 +1,13 @@
+package dev.cami.miprimerapirest.miprimerapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiprimerapirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiprimerapirestApplication.class, args);
+    }
+
+}
